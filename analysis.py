ae7723b1d309ee4d1cd428d324e58a723a048b5d
dvrinda <21f1001205@ds.study.iitm.ac.in>
2026-04-30 08:39:53 +0000

    Fix violations in analysis.py

--- analysis.py
+++ analysis.py
@@ -1,9 +1,6 @@
-import sys
-import os  # This is unsorted; 'os' should come after 'sys'
-
 def main():
-    
-    print("Hello, world!")  # No trailing whitespace here
+    print("Hello, world!")  # Removed unused variable and trailing whitespace
+
 
 if __name__ == "__main__":
-    main()  # This should be fine, no violations here
+    main()
