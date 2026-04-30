658f3d97f11aeb24ab705af74eb44d6279f6e581
dvrinda <21f1001205@ds.study.iitm.ac.in>
2026-04-30 07:09:09 +0000

    Fix violations in analysis.py

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import sys
 import os  # This is unsorted; 'os' should come after 'sys'
 
 def main():
-    unused_var = 10  # This variable is defined but not used anywhere
+    
     print("Hello, world!")  # No trailing whitespace here
 
 if __name__ == "__main__":
